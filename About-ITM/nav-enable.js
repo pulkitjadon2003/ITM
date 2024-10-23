6d5bb8c7284884674c7ce86afa11fa99887568d3
pulkitjadon2003 <pulkitjadon93@gmail.com>
2024-10-23 23:48:19 +0530

    About ITM check

--- About-ITM/nav-enable.js
+++ About-ITM/nav-enable.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             document.getElementById('nav-container').innerHTML = data;
             // Load the nav.js file after the HTML is loaded
+            let style = document.createElement('style')
+            style.src = '../Header_folder/nav.css'
+            document.body.appendChild(style)
             let script = document.createElement('script');
             script.src = '../Header_folder/nav.js';
             document.body.appendChild(script);
@@ -13,6 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('../Footer_folder/footer.html')  // Load the nav.html file
         .then(response => response.text()) // Get its content
         .then(data => {
+            let style = document.createElement('style')
+            style.src = '../Footer_folder/footer.css'
+            document.body.appendChild(style)
             document.getElementById('footer-container').innerHTML = data; // Insert into the page
         });
 }); 
