1df7d4837504314d07a90214cc83d8e38f4bdc20
pulkitjadon2003 <pulkitjadon93@gmail.com>
2024-10-23 21:46:21 +0530

    Nav is being shown

--- nav-enable.js
+++ nav-enable.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('Header_folder/nav.html')
+    fetch('./Header_folder/nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('nav-container').innerHTML = data;
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('Footer_folder/footer.html')  // Load the nav.html file
+    fetch('./Footer_folder/footer.html')  // Load the nav.html file
         .then(response => response.text()) // Get its content
         .then(data => {
             document.getElementById('footer-container').innerHTML = data; // Insert into the page
