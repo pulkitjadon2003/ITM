1ae2b67024f2d4ea824b02c5c435bd59e791d3f0
pulkitjadon2003 <pulkitjadon93@gmail.com>
2024-10-27 13:02:25 +0530

    Trying to fix the navbar

--- nav-enable.js
+++ nav-enable.js
@@ -1,13 +1,506 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('Header_folder/nav.html')
-        .then(response => response.text())
-        .then(data => {
+            let data = `        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="../font.css">
+    <nav>
+      <div id="nav1">
+        <div class="nav1-part1">
+          <div class="number"><p>+91 7047917816</p></div>
+          <div class="line"></div>
+          <div class="mail"><a href="#">saurabh109@gmail.com</a></div>
+        </div>
+        <div class="nav1-part2">
+          <!-- <div id="social"> -->
+          <p><i class="ri-youtube-line"></i>Youtube</p>
+          <p><i class="ri-facebook-fill"></i>Facebook</p>
+          <p>LinkdIn</p>
+          <a href="https://lms.itmgoi.in/" target="_blank"> <button>LMS</button></a>
+          <a href="http://mis.itmgoi.in/" target="_blank"><button>MIS</button></a>
+         
+          <!-- </div> -->
+        </div>
+      </div>
+      <div id="nav2">
+        <div id="navSec">
+          <ul id="outer-ul">
+            <!-- <li> -->
+              <a id="home" style="color: #503091" href="/index.html"
+                ><i class="fa-solid fa-house"></i
+              ></a>
+            <!-- </li> -->
+            <li>
+              <a id="list" href="About-ITM/about-ITM.html">About Institute</a>
+              <ul class="submenu submenu1">
+                <li><a href="About-ITM/about-ITM.html">About ITM</a></li>
+                <li><a href="About-ITM/visionAndMission.html">Vision, Mission & Values</a></li>
+                <li><a href="About-ITM/Chairman'sMessage.html">Message Board</a></li>
+                <li><a href="About-ITM/BOGs.html">Board of Governors</a></li>
+                <li><a href="About-ITM/ProgramOffered.html">Program Offered</a></li>
+                <li><a href="About-ITM/accredation.html">Accreditation</a></li>
+                <li>
+                  <a href="About-ITM/approval.html"
+                    >AICTE approvals & <br />
+                    Mandatory Disclosure</a
+                  >
+                </li>
+                <li><a href="About-ITM/infrastructure.html">Infrastructure</a></li>
+                <!-- <li><a href="About-ITM/Duties_And_Responsibilities.html">Duties & Responsibility</a></li> -->
+                <li><a href="About-ITM/ServiceRule.html">Service Rule</a></li>
+                <li><a href="About-ITM/faculty.html">Faculty</a></li>
+                <li><a href="About-ITM/Staff.html">Staff</a></li>
+                <li><a href="About-ITM/code of conduct.html">Code of Conduct <br />for Stackholder</a></li>
+                <li><a href="About-ITM/audit.html">Audit Reports</a></li>
+                <li><a href="About-ITM/policies.html">Policy</a></li>
+                <li><a href="About-ITM/Sop.html">Standards Operating Procedure</a></li>
+                <li><a href="About-ITM/gwaliorOffers.html">What Gwalior Offers</a></li>
+                <li><a href="About-ITM/contactUs.html">Contact Us</a></li>
+              </ul>
+            </li>
+            <li>
+              <a id="list" href="#">Administration</a>
+              <ul class="submenu">
+                <li><a href="Administration/adminis.html">Administrators</a></li>
+                <li><a href="Administration/IQAC/about.html">IQAC Internal Quality Assurance Cell</a></li>
+                <li><a href="Administration/antiRaging.html">Anti Ragging Committee</a></li>
+                <li><a href="Administration/Administrative.html">Administrative Responsibilites</a></li>
+                <!-- <li><a href="/Administration/committees.html" style="background-color:yellowgreen">Committees</a></li> -->
+              </ul>
+            </li>
+            <li>
+              <a id="list" href="#">Acadamic</a>
+              <ul class="submenu">
+                <li><a href="#">Departments</a>
+                  <ul class="submenu2">
+                    <li><a href="Academics/EngineeringScienceandHumanities/about.html">Engineering Science and Humanities</a></li>                    
+                    <li><a href="Academics/CSE/about.html">Computer Science and Engineering</a></li>
+                    <li><a href="Academics/IT/about.html">Information Technology</a></li>
+                    <li><a href="Academics/EC/about.html">Electronics and Communication</a></li>
+                    <li><a href="Academics/CE/about.html">Civil Engineering</a></li>
+                    <li><a href="Academics/ME/about.html">Mechanical Engineering</a></li>
+                    <li><a href="Academics/MBA/about.html">MBA</a></li>
+                    <li><a href="Academics/MCA/about.html">MCA</a></li>
+                  </ul>
+                </li>
+                <li><a href="">Courses</a>
+                  <ul class="submenu2">
+                    <li><a href=Admission_folder/UG.html">Under Graduate</a></li>
+                    <li><a href=Admission_folder/PG.html">Post Graduate</a></li>
+                  </ul>
+                </li>
+                <li><a href="Academics/AcademicCalender/AcademicCal.html">Acadamic Calender</a></li>
+                <li><a href="Academics/QualityInitiative.html">Quality Initiatives</a></li>
+                <li><a href="Academics/curriculum.html">Curriculum Feedback</a></li>
+                <!-- <li><a href="" style="background-color: greenyellow;">LMS</a> -->
+                  <ul class="submenu2">
+                    <a href="Academics/LMS/mentorMenteeScheme.html">Mentor Mentee Scheme</a>
+                    <a href="https://lms.itmgoi.in/" target="_blank">LMS(Tattva)</a>
+                    <a href="http://mis.itmgoi.in/" target="_blank">MIS(Prabandh)</a>
+
+                  </ul>
+                </li>
+                <!-- <li><a href="https://web.mitsgwalior.in/images/mainmenu/OBE%20Manual.pdf" style="background-color: greenyellow;">Outcome Based Mannual</a></li> -->
+              </ul>
+            </li>
+            <li>
+              <a id="list" href="Life@ITM/skill-enhancement-activities.html">Life @ITM</a>
+              <ul class="submenu">
+                <li><a href="Life@ITM/skill-enhancement-activities.html">Facilities</a>
+                  
+                  <ul class="submenu2">
+                    <li><a href="Life@ITM/skill-enhancement-activities.html">Skill Enhancement Activies</a></li>
+                    <li><a href="Life@ITM/learning.html">Learning Resource Centre</a></li>
+                    <li><a href="Life@ITM/sportscell.html">Sport Cell</a></li>
+                    <li><a href="Life@ITM/medical.html">Medical & Dispensary</a></li>
+                    <li><a href="Life@ITM/workshop.html">Workshop</a></li>
+                    <li><a href="Life@ITM/Gymnesium.html">Gymnesium</a></li>
+                    <!-- <li><a href="Life@ITM/guesthouse.html" style="background-color: greenyellow;">Guest House</a></li> -->
+                    <!-- <li><a href="Life@ITM/" style="background-color: greenyellow;">Canteen</a></li> -->
+                  </ul>
+
+                </li>
+                <li><a href="About-ITM/magazine.html">Student Magazine</a></li>
+                <li><a href="">Campus Life</a>
+                  <ul class="submenu2">
+                    <li><a href="Life@ITM/hostel.html">Hostel Details</a></li>
+                     <li><a href="/">Code of Conduct for Students</a></li>
+                     <li><a href="/">Hostel Timing</a></li>
+                     <li><a href="/">Hostel and Mess Fee</a></li>
+                     <li><a href="/">Hostel Registration, etc</a></li>
+                  </ul>
+                </li>
+                <li><a href="Life@ITM/club.html">Student Activities</a>
+                  <ul class="submenu2">
+                   <li> <a href="Life@ITM/club.html">Societies & Clubs</a></li>
+                    <!-- <li><a href="Life@ITM/ncc.html">NCC</a></li> -->
+                    <li><a href="Life@ITM/nss.html">NSS</a></li>
+                    <li><a href="">Commemorative Days</a></li>
+                    <li><a href="Life@ITM/sports.html">Sports and Cultural Events</a></li>
+                    <!-- <li><a href="Life@ITM/Commemorative.html" style="background-color: greenyellow;">National/International Commemorative days/ events</a></li> -->
+
+                  </ul></li>
+                <li>
+                  <a href="Life@ITM/grievance/ragging.html"
+                    >Student Grievance & <br>
+                    Counselling</a
+                  >
+
+                </li>
+                  <li><a href="Life@ITM/EnvironmentalInitiatives.html">Environmental Initiatives</a></li>
+                  <li><a href="">Cultural Exchange Programs</a></li>
+                  <li><a href="Life@ITM/studentAchievements.html">Student Achievements</a></li>
+                  <li><a href="">Community Engagement</a></li>
+                
+              </ul>
+            </li>
+           
+            <li>
+              <a id="list" href="R&D/startup.html">R&D</a>
+              <ul class="submenu">
+                <li><a href="R&D/startup.html">Startup / Innovation / Incubation cell</a></li>
+                <li><a href="R&D/Mou.html">MoUs & Collaborations</a></li>
+                <li><a href="R&D/patents.html">Patents</a></li>
+                <li><a href="R&D/R&Dactivities.html">R & D Activities</a></li>
+                <li><a href="R&D/publication.html">Publications</a></li>
+                <li><a href="R&D/industry.html">Industrial Visits</a></li>
+                <li><a href="R&D/projectByStudents.html">Projects By Students</a></li>
+                <li><a href="">Ideapad</a></li>
+
+              </ul>
+            </li>
+            <li>
+              <a id="list" href="Tap&Placement/T&pCell.html">TAP</a>
+              <!-- <ul class="submenu submenu1">
+                <li><a href="#">Internship and Placement Guidelines - Policies</a></li>
+                <li><a href="#">Career Counselling Services</a></li>
+                <li><a href="#">Skill Development Activies & Events</a></li>
+                <li><a href="#">Workshop & Seminars</a></li>
+                <li><a href="#">Placement Preparartion Resources</a></li>
+                <li><a href="#">Industry Collabrations</a></li>
+                <li><a href="#">Top Recruiters</a></li>
+                <li><a href="#">Internship & Placements Statistics</a></li>
+                <li><a href="#">Student Success Storys</a></li>
+                <li><a href="#">Internship & Placements Policy</a></li>
+                <li><a href="#">Notable Alumni</a></li>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Contact (TAP)</a></li>
+                <li><a href="#" style="background-color: greenyellow;">About T&P Cell</a></li>
+              </ul> -->
+            </li>
+            <li>
+              <a href="Alumni/menualumni.html">Alumni</a>
+            </li>
+            <li>
+              <a id="list" href="#">Admission</a>
+              <ul class="submenu">
+                <li><a href="Admission_folder/prosp.html">Prospectus</a></li>
+                <li><a href="Admission_folder/UG.html">Under Graduate Admission</a></li>
+                <li><a href="Admission_folder/PG.html">Post Graduate Admission</a></li>
+                <li><a href="">Fee Structure</a></li>
+                <li><a href="http://itmgoi.in/OnlineApply_ITMGOI/">Enquiry Form</a></li>
+                <li><a href="https://onlineapply.itmgoi.in/form_hdfc.php?ok=Apply+Now">Registration Form</a></li>
+                <li><a href="Admission_folder/seek_admission.html">How to Seek Admission</a></li>
+                <li><a href="Admission_folder/FAQ.html">FAQs</a></li>
+                <li><a href="Admission_folder/contact.html">Contact (Admission)</a></li>
+              </ul>
+            </li>
+            <li>
+              <a id="list" href="#">Gallery</a>
+              <ul class="submenu">
+                <li>
+                  <a href="Gallery_folder/photoGallery1.html"
+                    >Photo Gallery</a
+                  >
+                </li>
+                <li><a href="#">Video Gallery</a></li>
+              </ul>
+            </li>
+            <li>
+              <a  id="list"href="#">NAAC</a>
+              <ul class="submenu">
+                <li><a href="#">Item1</a></li>
+                <li><a href="#">Item2</a></li>
+                <li><a href="#">Item3</a></li>
+                <li><a href="#">Item4</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="logo">
+        <img src="images/itm logo.png" alt="" />
+      </div>
+
+      <!-- <div id="img">
+        <img src="./dunedin-university-of.jpg" alt="">
+        <div id="nameDiv">
+            <h2>Department Of Civil Engineering</h2>
+            <div id="line"></div>
+
+        </div>
+    </div> -->
+    </nav>`
             document.getElementById('nav-container').innerHTML = data;
+            
             // Load the nav.js file after the HTML is loaded
+            let style = document.createElement('style')
+            style.innerHTML = `* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: gilroy;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+nav {
+  height: 100px;
+  width: 100vw;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.368);
+  position: relative;
+
+}
+#nav1 {
+  height: 30px;
+  width: 100%;
+  display: flex;
+  position: relative;
+  background-color: #1e0258;
+  color: #fff;
+}
+
+.nav1-part1 {
+  width: 24%;
+  height: 100%;
+  padding: 0 5px;
+  /* background-color: orange; */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  left: 22%;
+}
+.number p {
+  font-size: 15px;
+  font-weight: 300;
+  color: #fff;
+  opacity: 0.6;
+}
+.line {
+  width: 2px;
+  height: 60%;
+  background-color: #503091;
+  opacity: 0.6;
+}
+.mail a {
+  font-size: 15px;
+  font-weight: 300;
+  text-decoration: none;
+  color: salmon;
+  opacity: 0.6;
+}
+
+.nav1-part2 {
+  width: 25%;
+  /* background-color: orange; */
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: space-around;
+  position: absolute;
+  right: 10%;
+}
+.nav1-part2 p {
+  font-size: 15px;
+  font-weight: 300;
+  text-decoration: none;
+  color: #fff;
+  opacity: 0.6;
+}
+.nav1-part2 button{
+  padding: 2px 10px ;
+  border-radius: 5px;
+  border: none;
+  color: #503091;
+  background-color: white;
+  font-weight: 900;
+}
+.nav1-part2 button:hover{
+  color: white;
+  background-color: #503091;
+  transition: all ease-in-out .6s;
+  
+}
+
+nav #logo {
+  width: 140px;
+  height: 120px;
+  background-color: #503091;
+  position: absolute;
+  top: 0;
+  /* padding:18px;
+    padding-top: 30px; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 6%;
+  /* border:2px solid black; */
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  z-index: 999999;
+}
+nav #logo img {
+  height: 55%;
+  object-fit: cover;
+}
+
+#nav2 {
+  height: 60px;
+  width: 100%;
+  border-top: 1px solid black;
+  position: relative;
+}
+
+
+#navSec {
+  width: 75%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 12px;
+  margin-left: 20%;
+}
+#navSec #outer-ul {
+  list-style: none;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+}
+#navSec ul li {
+  display: inline-block;
+  list-style: none;
+  font-size: 18px;
+  font-weight: 500;
+  position: relative;
+}
+
+#navSec ul li a {
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+  display: inline-block;
+  font-size: 15px;
+  padding: 5px;
+  transition: background-color ease .4s;
+  width: 100%;
+  height: 100%;
+}
+#navSec ul li a:hover    {
+  background-color: #ffc400  !important;
+}
+
+#navSec li #home:hover{
+  background-color: transparent !important;
+}
+
+
+/* submenu style */
+#navSec ul .submenu {
+  position: absolute;
+  background-color: #f9f9f9;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  display: grid;
+  grid-row-gap: 10px;
+  width: 192px;
+  grid-template-columns: auto;
+  z-index: -999;
+  padding: 5px;
+  margin-top: -1px;
+  opacity: 0;
+  border-radius: 4px;
+  margin-left: -6.5vw;
+}
+
+#navSec ul li:hover .submenu {
+  z-index: 999999999999;
+  opacity: 1;
+  
+}
+#navSec ul .submenu li {
+  margin: 0;
+  width: 100%;
+  transition: all ease .2s;
+}
+
+#navSec ul .submenu li a {
+  padding: 8px 12px;
+  display: inline-block;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.09);
+}
+
+#navSec ul .submenu2 {
+  position: absolute;
+  background-color: #f9f9f9;
+  box-shadow: 0 20px 45px #00000020;
+  width: 200px;
+  display: inline-block;
+  grid-row-gap: 10px;
+  grid-template-columns: auto;
+  z-index: -9;
+  padding: 5px;
+  opacity: 0;
+  transition: all ease-in-out 0.4s;
+}
+
+#navSec ul .submenu li:hover .submenu2 {
+  z-index: 99;
+  opacity: 1;
+}
+#navSec ul .submenu>li:hover{
+  background-color: #ffc400;
+}
+
+#navSec ul li:hover{
+  background-color: #ffc400;
+}
+
+
+#navSec ul .submenu2 li {
+  margin: 0;
+  width: 100%;
+  /* display: inline; */
+}
+
+#navSec ul .submenu2 li a {
+  padding: 8px 12px;
+  display: inline-block;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.09);
+}
+
+`
+            document.body.appendChild(style);
             let script = document.createElement('script');
-            script.src = 'Header_folder/nav.js';
+            script.innerHTML = `var list = document.querySelectorAll("#list");
+                                var submenu1 = document.querySelectorAll(".submenu1");
+
+                                list.forEach((e) => {
+                                e.addEventListener("mouseenter", () => {
+                                    submenu1.forEach((submenu) => {
+                                    submenu.style.gridTemplateColumns = "auto auto";
+                                    submenu.style.width = "330px"
+                                    });
+                                });
+                                });`;
             document.body.appendChild(script);
-        });
+            // console.log(script);
+            
 });
 document.addEventListener("DOMContentLoaded", function () {
     fetch('Footer_folder/footer.html')  // Load the nav.html file
